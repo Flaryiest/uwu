8ba375b0db77a98af264e573c10a7dd0d8cc68bb
Flaryiest <ericmzuo1@gmail.com>
2025-02-15 09:48:12 -0700

    Update indexPage.jsx

--- frontend/src/pages/indexPage.jsx
+++ frontend/src/pages/indexPage.jsx
@@ -1,7 +1,19 @@
 import { useEffect } from 'react';
 export default function IndexPage() {
     useEffect(() => {
-        console.log('Pog')
+        async function getInfo() {
+            const response = await fetch(
+              "https://computing4charity-frontend-production.up.railway.app/api/user",
+              {
+                method: "POST",
+                credentials: "include",
+              },
+            );
+            const userInfo = await response.json();
+            console.log(userInfo, "userinfo");
+            setUserInfo(userInfo);
+          }
+          getInfo();
     })
     return <div>
         <p>Pog</p>
